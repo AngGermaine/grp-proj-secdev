154e3bea437605879509d2d79ec61055e0dbeb06
AngGermaine <germaine_richie_ang@dlsu.edu.ph>
2026-04-05 02:02:00 +0800

    Updated dashboard and modals
    Both js and html so the forms are working

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,8 +1,12 @@
 // Edit Modal
-function openEditModal(name, value, qty) {
+function openEditModal(id, name, value, qty) {
+    document.getElementById('editId').value = id;
     document.getElementById('editName').value = name;
     document.getElementById('editValue').value = value;
     document.getElementById('editQty').value = qty;
+
+    const form = document.getElementById('editAssetForm');
+    form.action = `/assets/${id}/edit`;
 
     const modal = document.getElementById('editModal');
     modal.classList.remove('hidden');
